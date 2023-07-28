63749193123c63f7cd7add307f73409ecbcddf0e
Vivek <vivekmalkapuram@gmail.com>
2023-07-28 13:44:41 +0530

    Added assessment repo content

---
+++ service/api/views.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions, viewsets
+
+from . import models, serializers
+
+
+class TrackViewSet(viewsets.ModelViewSet):
+    queryset = models.Track.objects.all()
+    serializer_class = serializers.TrackSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
